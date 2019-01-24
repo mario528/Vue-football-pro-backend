9fb5f59a1eae9683cedd5192d61ed994cc7b3684
majiaao <majiaao@wecash.net>
2019-01-24 15:43:46 +0800

    添加注册模块

--- service/index.js
+++ service/index.js
@@ -1,4 +1,6 @@
 const login = require('./login')
+const register = require('./register')
 module.exports = (app) => {
     app.use(login)
+    app.use(register)
 }
--- service/register.js
+++ service/register.js
@@ -8,4 +8,12 @@ router.post('/register', (req,res)=> {
     const md5 = crypto.createHash('md5');
     const username = req.body.username;
     const password = md5.update(req.body.password).digest('base64');
+    const telephone = req.body.telephone;
+    console.log(username,password,telephone)
+    DB.insertOne('user',
+    {'username': username , 'password': password, 'phoneNumber': telephone },(err,result)=> {
+        if(err) 
+            console.log(err);
+    })
 })
+module.exports = router;
